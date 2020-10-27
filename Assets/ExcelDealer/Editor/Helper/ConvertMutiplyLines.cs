7a82b5d324520e8593f7f1a90e4eb7fe1969b910
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-27 14:12:45 +0800

    提交

---
+++ Assets/ExcelDealer/Editor/Helper/ConvertMutiplyLines.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using UnityEngine;
+public class ConvertMutiplyLines : IDealExcel
+{
+    public static string projectFloader = "";
+    public string floader { get; set; } =$"{projectFloader}Config";
+
+    public string fileName { get; set; }
+    public string divededStr = "/";
+    int startCol;
+    int colCount;
+    int startRow;
+    public ConvertMutiplyLines(string fileName, int startCol, int colCount, bool ignoreHead = true)
+    {
+        startRow = ignoreHead ? 1 : 0;
+        this.startCol = startCol;
+        this.colCount = colCount;
+        this.fileName = $"{fileName}.txt";
+    }
+
+    public string Run(DataTable reader)
+    {
+        StringBuilder builder = new StringBuilder();
+        int count = startCol + colCount;
+        for (int i = startRow; i < reader.Rows.Count; i++)
+        {
+            var item = reader.Rows[i].ItemArray;
+            for (int j = startCol; j < count; j++)
+            {
+                //Debug.Log($"{j},{colCount}");
+                builder.Append(item[j].ToString());
+                if (j < count - 1)
+                    builder.Append(divededStr);
+            }
+            if (i < reader.Rows.Count - 1)
+                builder.Append("\n");
+        }
+        return builder.ToString();
+    }
+}
+
---
+++ Assets/ExcelDealer/Editor/Helper/ConvertToByteArray.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class ConvertToByteArray : IDealExcel
+{
+    public string floader => throw new System.NotImplementedException();
+
+    public string fileName => throw new System.NotImplementedException();
+
+    public string Run(DataTable reader)
+    {
+        throw new System.NotImplementedException();
+    }
+}
